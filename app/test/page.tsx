1d952ea723162677d99a88fe8445a177e3345213
JeongwooSeo <shipfriend4371@naver.com>
2024-11-17 23:09:06 +0900

    chore: 로티 애니메이션 라이브러리 설치

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Lottie from 'lottie-react';
+import snowman from './snowman.json'
+
+const TestPage = () => {
+
+
+  return <Lottie animationData={snowman}  />
+
+}
+
+export default TestPage
