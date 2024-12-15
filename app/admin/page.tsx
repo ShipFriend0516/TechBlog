913d682aafc8551e8ee05d2e07e0bb90d4b027b6
JeongwooSeo <shipfriend4371@naver.com>
2024-12-15 17:34:29 +0900

    feat: 비로그인시 화면 구현

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -15,9 +15,13 @@ const AdminDashboard = () => {
 
   if (!session) {
     return (
-      <div className="flex justify-center items-center h-screen">
+      <header className="mb-8 h-96 flex flex-col gap-4 justify-center items-center">
+        <div>
+          <h1 className="text-3xl font-bold mb-2">관리자 대시보드</h1>
+          <p className="text-gray-200">로그인이 필요한 기능입니다.</p>
+        </div>
         <GithubLogin signIn={signIn} />
-      </div>
+      </header>
     );
   }
 
--- app/entities/common/Button/GithubLogin.tsx
+++ app/entities/common/Button/GithubLogin.tsx
@@ -1,3 +1,5 @@
+import { FaGithub } from 'react-icons/fa';
+
 interface GithubLoginProps {
   signIn: () => void;
   text?: string;
@@ -5,9 +7,10 @@ interface GithubLoginProps {
 const GithubLogin = ({ signIn, text }: GithubLoginProps) => {
   return (
     <button
-      className="px-8 py-2 text-2xl bg-black text-white rounded-md shadow-md hover:bg-gray-800 transition-all"
+      className="px-5 py-3 text-2xl bg-black text-white rounded-md shadow-md hover:bg-gray-600 transition-all inline-flex items-center gap-2"
       onClick={signIn}
     >
+      <FaGithub />
       {text ? text : 'GitHub로 로그인'}
     </button>
   );
