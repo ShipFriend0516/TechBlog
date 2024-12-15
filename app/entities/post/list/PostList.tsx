55fabfe8751931d31fb3e9fbe8bde312af992153
JeongwooSeo <shipfriend4371@naver.com>
2024-12-15 23:59:56 +0900

    refactor: 최대 너비 증가

--- app/entities/post/list/PostList.tsx
+++ app/entities/post/list/PostList.tsx
@@ -7,7 +7,7 @@ const PostList = (props: { loading: boolean; posts: Post[] | undefined }) => {
   return (
     <ul
       className={
-        'max-w-5xl mx-auto post-list my-12 px-4 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6'
+        'max-w-6xl mx-auto post-list my-12 px-4 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6'
       }
     >
       {props.loading ? (
