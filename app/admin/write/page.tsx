604304d410c4ac6a44b179cc117bfcae617f31b4
JeongwooSeo <shipfriend4371@naver.com>
2024-12-18 02:33:27 +0900

    feat: 글발행 성공시 posts로 이동

--- app/admin/write/page.tsx
+++ app/admin/write/page.tsx
@@ -2,15 +2,20 @@
 import BlogForm from '@/app/entities/post/write/BlogForm';
 import axios from 'axios';
 import { Post } from '@/app/types/Post';
+import { useRouter } from 'next/navigation';
 
 type PostBody = Omit<Post, '_id' | 'date' | 'timeToRead' | 'comment'>;
 
 const BlogWritePage = () => {
+  const router = useRouter();
   const postBlog = async (post: PostBody) => {
     try {
       const response = await axios.post('/api/posts', post);
       const data = await response.data;
       console.log('글 발행 결과', data);
+      if (response.status === 201) {
+        router.push('/posts');
+      }
     } catch (e) {
       console.error('글 발행 중 오류 발생', e);
     }
