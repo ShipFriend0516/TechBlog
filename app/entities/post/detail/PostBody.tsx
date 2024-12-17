0a58963eb9d58313b9963ad6136529750897d373
JeongwooSeo <shipfriend4371@naver.com>
2024-12-18 02:04:16 +0900

    feat: Markdown Preview 컴포넌트로 md 형식 렌더링

--- app/entities/post/detail/PostBody.tsx
+++ app/entities/post/detail/PostBody.tsx
@@ -1,4 +1,5 @@
 import LoadingIndicator from '@/app/entities/common/Loading/LoadingIndicator';
+import MDEditor from '@uiw/react-md-editor';
 
 interface Props {
   content: string;
@@ -7,8 +8,30 @@ interface Props {
 
 const PostBody = ({ content, loading }: Props) => {
   return (
-    <div className={'post-body px-4 py-16 pb-52 whitespace-pre-wrap'}>
-      {loading ? <LoadingIndicator /> : content}
+    <div className={'w-full'}>
+      {loading ? (
+        <div className={'w-1/2'}>
+          <LoadingIndicator />
+        </div>
+      ) : (
+        <MDEditor.Markdown
+          style={{
+            backgroundColor: 'var(--background)',
+            color: 'var(--text-primary)',
+          }}
+          className={'post-body px-4 py-16 pb-52 whitespace-pre-wrap'}
+          source={content}
+          rehypeRewrite={(node, index, parent) => {
+            if (
+              node.tagName === 'a' &&
+              parent &&
+              /^h(1|2|3|4|5|6)/.test(parent.tagName)
+            ) {
+              parent.children = parent.children.slice(1);
+            }
+          }}
+        />
+      )}
     </div>
   );
 };
