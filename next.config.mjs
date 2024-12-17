d31c695383aea5b635c2e3c0a09c60fb3a08ccf2
JeongwooSeo <shipfriend4371@naver.com>
2024-12-18 03:00:15 +0900

    chore: next.config.mjs 에서 images.domain 속성이 deprecated 되어서 remotePatterns로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['idmlft3uczstcjaa.public.blob.vercel-storage.com', '*'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'idmlft3uczstcjaa.public.blob.vercel-storage.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+    ],
   },
 };
 
