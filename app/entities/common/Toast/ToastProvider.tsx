25a5834434ddd16c7c3b5d76e96d36073694cb63
JeongwooSeo <shipfriend4371@naver.com>
2024-12-19 02:43:39 +0900

    feat: 토스트 컴포넌트, 프로바이더, 훅, 스토어 구현

---
+++ app/entities/common/Toast/ToastProvider.tsx
@@ -0,0 +1,30 @@
+'use client';
+import Toast from '@/app/entities/common/Toast/Toast';
+import useToastStore from '@/app/stores/useToastStore';
+
+interface Toast {
+  id: number;
+  message: string;
+  type: 'success' | 'error';
+}
+
+const ToastProvider = () => {
+  const { toasts, removeToast } = useToastStore();
+
+  return (
+    <div className={'fixed z-50 flex flex-col gap-2 top-10 right-10'}>
+      {toasts.map((toast: Toast) => {
+        return (
+          <Toast
+            key={toast.id}
+            removeToast={() => removeToast(toast.id)}
+            message={toast.message}
+            type={toast.type}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default ToastProvider;
---
+++ app/hooks/useToast.ts
@@ -0,0 +1,50 @@
+import useToastStore from '@/app/stores/useToastStore';
+import { useCallback, useEffect, useRef } from 'react';
+
+const useToast = () => {
+  const { createToast, removeToast } = useToastStore();
+  const DURATION = 5000;
+  const timerIDs = useRef<ReturnType<typeof setTimeout>[]>([]);
+
+  const toast = useCallback(
+    (message: string, type: 'success' | 'error') => {
+      const newToast = {
+        id: new Date().getTime() + Math.floor(Math.random() * 200),
+        message,
+        type,
+        duration: DURATION || 5000,
+      };
+
+      createToast(newToast);
+      const id = setTimeout(() => {
+        removeToast(newToast.id);
+        timerIDs.current = timerIDs.current.filter((timerId) => timerId !== id);
+      }, DURATION);
+    },
+    [createToast, removeToast]
+  );
+
+  useEffect(() => {
+    return () => {
+      timerIDs.current.forEach((id) => clearTimeout(id));
+    };
+  }, []);
+
+  const success = useCallback(
+    (message: string) => {
+      toast(message, 'success');
+    },
+    [toast]
+  );
+
+  const error = useCallback(
+    (message: string) => {
+      toast(message, 'error');
+    },
+    [toast]
+  );
+
+  return { success, error };
+};
+
+export default useToast;
