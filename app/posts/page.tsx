b20a5cc4b0f84a0f1a64ca9961c0ef4a7aa0713b
JeongwooSeo <shipfriend4371@naver.com>
2024-12-23 01:10:17 +0900

    fix: debounce가 적용되지 않던 문제 해결

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,26 +1,28 @@
 'use client';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Post } from '@/app/types/Post';
 import PostList from '@/app/entities/post/list/PostList';
 import SearchSection from '@/app/entities/post/list/SearchSection';
 import { debounce } from 'lodash';
+import useSearchQueryStore from '@/app/stores/useSearchQueryStore';
 
 const BlogList = () => {
   const [posts, setPosts] = useState<Post[]>();
   const [loading, setLoading] = useState(true);
   const [query, setQuery] = useState('');
-
+  const addLatestQuery = useSearchQueryStore((state) => state.addSearchQuery);
   const getPosts = async (query?: string) => {
     const response = await axios.get(
       `/api/posts${query ? `?query=${query}` : ''}`
     );
     const data = await response.data;
     setPosts(data.posts);
+    if (query) addLatestQuery(query);
     setLoading(false);
   };
 
-  const debouncedGetPosts = debounce(getPosts, 500);
+  const debouncedGetPosts = useCallback(debounce(getPosts, 500), []);
 
   useEffect(() => {
     debouncedGetPosts(query);
