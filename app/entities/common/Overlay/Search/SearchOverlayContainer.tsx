9626de6ce8cd62bdcb610b692d80b66fe192f48b
JeongwooSeo <shipfriend4371@naver.com>
2024-12-23 00:03:41 +0900

    feat: 검색어 지우는 버튼 추가

--- app/entities/common/Overlay/Search/SearchOverlayContainer.tsx
+++ app/entities/common/Overlay/Search/SearchOverlayContainer.tsx
@@ -1,5 +1,6 @@
 import { FaSearch } from 'react-icons/fa';
 import Tag from '@/app/entities/common/Tag';
+import { FaX } from 'react-icons/fa6';
 
 const SearchOverlayContainer = (props: {
   setQuery: (query: string) => void;
@@ -7,18 +8,29 @@ const SearchOverlayContainer = (props: {
   onCancel: () => void;
   tags: string[];
 }) => {
+  const emptyInput = () => {
+    props.setQuery('');
+  };
   return (
     <div className="px-5 p-4">
-      <div className="flex items-center space-x-4 mb-4">
-        <FaSearch size={20} className="text-gray-400" />
-        <input
-          type="text"
-          placeholder="검색어를 입력하세요..."
-          className="w-full p-2 outline-none"
-          autoFocus
-          onChange={(e) => props.setQuery(e.target.value)}
-          value={props.value}
-        />
+      <div className="flex mb-4">
+        <div className={'flex flex-grow items-center space-x-4 relative'}>
+          <FaSearch size={20} className="text-gray-400" />
+          <input
+            type="text"
+            placeholder="검색어를 입력하세요..."
+            className="w-full p-2 outline-none"
+            autoFocus
+            onChange={(e) => props.setQuery(e.target.value)}
+            value={props.value}
+          />
+          <button
+            className={`${props.value ? 'block' : 'hidden'} p-2 text-gray-400 absolute right-2`}
+            onClick={emptyInput}
+          >
+            <FaX />
+          </button>
+        </div>
         <button
           onClick={props.onCancel}
           className="text-gray-500 hover:text-gray-700 p-2"
