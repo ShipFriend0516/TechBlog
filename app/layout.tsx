081bc347ebc1f7d90d1467e14a6bd3c18fae9867
JeongwooSeo <shipfriend4371@naver.com>
2024-12-22 15:04:09 +0900

    feat: 사용하지 않는 폰트 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,17 +6,6 @@ import React from 'react';
 import NavBar from '@/app/entities/common/NavBar';
 import ToastProvider from '@/app/entities/common/Toast/ToastProvider';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
-
 export const metadata: Metadata = {
   title: 'ShipFriend TechBlog',
   description: '문제 해결 경험과 개발 지식을 공유하는 개발 블로그입니다.',
@@ -33,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body
-        className={`dark ${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col justify-between`}
+        className={`dark antialiased min-h-screen flex flex-col justify-between`}
       >
         <NavBar />
         <main className="flex-grow pb-20">{children}</main>
