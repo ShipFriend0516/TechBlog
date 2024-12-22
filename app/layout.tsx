be9762b9bf707e6cdfcb72187f8225901a89c8af
JeongwooSeo <shipfriend4371@naver.com>
2024-12-22 15:18:55 +0900

    feat: pretendard localfont 최적화

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,57 @@ export const metadata: Metadata = {
   },
 };
 
+const pretendard = localFont({
+  src: [
+    {
+      path: './fonts/woff2/Pretendard-Thin.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: './fonts/woff2/Pretendard-ExtraLight.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: './fonts/woff2/Pretendard-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/woff2/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/woff2/Pretendard-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/woff2/Pretendard-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/woff2/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/woff2/Pretendard-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: './fonts/woff2/Pretendard-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-pretendard',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -22,7 +73,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body
-        className={`dark antialiased min-h-screen flex flex-col justify-between`}
+        className={`dark ${pretendard.variable} font-sans antialiased min-h-screen flex flex-col justify-between`}
       >
         <NavBar />
         <main className="flex-grow pb-20">{children}</main>
