f3938eff340b76a18bbb23e1ac72f3942736bf30
JeongwooSeo <shipfriend4371@naver.com>
2024-12-25 18:13:32 +0900

    fix: 글 생성, 수정, 삭제시 series의 posts 업데이트

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,6 +3,7 @@ import dbConnect from '@/app/lib/dbConnect';
 import { getServerSession } from 'next-auth/next';
 import { getThumbnailInMarkdown } from '@/app/lib/utils/parse';
 import { generateUniqueSlug } from '@/app/lib/utils/post';
+import Series from '@/app/models/Series';
 
 // GET /api/posts - 모든 글 조회
 export async function GET(req: Request) {
@@ -45,8 +46,15 @@ export async function POST(req: Request) {
     }
 
     await dbConnect();
-    const { title, subTitle, author, content, profileImage, thumbnailImage } =
-      await req.json();
+    const {
+      title,
+      subTitle,
+      author,
+      content,
+      profileImage,
+      thumbnailImage,
+      seriesId,
+    } = await req.json();
 
     if (!title || !content || !author || !content) {
       return Response.json(
@@ -64,6 +72,7 @@ export async function POST(req: Request) {
       timeToRead: Math.ceil(content.length / 500),
       profileImage,
       thumbnailImage: thumbnailImage || getThumbnailInMarkdown(content),
+      seriesId: seriesId || null,
     };
 
     const newPost = await Post.create(post);
@@ -72,6 +81,13 @@ export async function POST(req: Request) {
         { success: false, error: '글 작성 실패' },
         { status: 500 }
       );
+    }
+
+    if (post.seriesId) {
+      await Series.findByIdAndUpdate(post.seriesId, {
+        $push: { posts: newPost._id },
+        $inc: { postCount: 1 }, // postCount 1 증가
+      });
     }
 
     return Response.json(
