c4156bd1faf614a38083bd3f700a4f098aa83ca6
JeongwooSeo <shipfriend4371@naver.com>
2025-04-04 21:11:57 +0900

    feat: 정적 에셋 폴더 구조 변경

--- app/portfolio/data.ts
+++ app/portfolio/data.ts
@@ -1,22 +1,19 @@
 import { Project } from '@/app/types/Portfolio';
-import project1 from '@/app/public/images/preview-logo.png';
-import project2 from '@/app/public/images/primitive-logo.webp';
-import project3 from '@/app/public/images/shipfriend-logo.webp';
-import project4 from '@/app/public/images/storyhelper-logo.webp';
 
 export const projects: Project[] = [
   {
     title: 'PREVIEW',
     description: 'WebRTC 기반 화상 면접 스터디 플랫폼',
-    image: project1,
+    image: '/images/logo/preview-logo.png',
     tags: ['React', 'WebRTC', 'Fullstack'],
     demoUrl: 'https://boostcamp-preview.kro.kr/',
     githubUrl: 'https://github.com/boostcampwm-2024/web27-Preview',
+    slug: 'preview',
   },
   {
     title: 'Primitive',
     description: '프로그래밍 동아리 PRIMITIVE 홍보 및 프로젝트 공유 플랫폼',
-    image: project2,
+    image: '/images/logo/primitive-logo.webp',
     tags: ['React', 'TypeScript', 'Firebase'],
     demoUrl: 'https://primitive.kr',
     githubUrl: 'https://github.com/ShipFriend0516/Primitive',
@@ -25,18 +22,20 @@ export const projects: Project[] = [
   {
     title: 'ShipFriend TechBlog',
     description: '개인 개발 블로그',
-    image: project3,
+    image: '/images/logo/shipfriend-logo.webp',
     tags: ['Next.js', 'TypeScript', 'MongoDB'],
     demoUrl: 'https://shipfriend.vercel.app/',
     githubUrl: 'https://github.com/ShipFriend0516/TechBlog',
+    slug: 'shipfriend',
   },
   {
     title: 'StoryHelper',
     description: '티스토리 블로그 SEO 관리 확장프로그램',
-    image: project4,
+    image: '/images/logo/storyhelper-logo.webp',
     tags: ['React', 'Chrome Extension'],
     demoUrl:
       'https://chromewebstore.google.com/detail/storyhelper/inmbdknioncgblpeiiohmdihhidnjpfp?authuser=0&hl=ko',
     githubUrl: 'https://github.com/ShipFriend0516/StoryHelper',
+    slug: 'storyhelper',
   },
 ];
