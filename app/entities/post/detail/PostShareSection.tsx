52187c8ebc1a7f42efaefc798ca7f1db8ff40cbd
JeongwooSeo <shipfriend4371@naver.com>
2025-04-05 11:02:28 +0900

    style: post share component style

--- app/entities/post/detail/PostShareSection.tsx
+++ app/entities/post/detail/PostShareSection.tsx
@@ -8,14 +8,13 @@ const PostShareSection = () => {
   const sharePost = async () => {
     const url = window.location.href;
     await navigator.clipboard.writeText(url);
-
     toast.success('링크가 복사되었습니다.');
   };
 
   return (
     <section
       className={
-        'flex justify-start items-center w-full max-w-3xl mx-auto border-t border-neutral-200 py-4'
+        'flex justify-start items-center w-full max-w-3xl mx-auto border-t border-neutral-200 px-4 py-4'
       }
     >
       {/* 공유 버튼 */}
@@ -23,7 +22,7 @@ const PostShareSection = () => {
         <button
           onClick={() => sharePost()}
           className={
-            'inline-flex items-center gap-2 rounded-md border p-1 hover:bg-neutral-100 border-neutral-400'
+            'inline-flex items-center gap-2 rounded-md border p-1 hover:bg-neutral-100 border-neutral-200'
           }
         >
           <MdIosShare />
