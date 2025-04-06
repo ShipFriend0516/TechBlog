ea6bf2bc96a460c475712ba4d263dd62ab3e6376
JeongwooSeo <shipfriend4371@naver.com>
2025-04-06 16:33:59 +0900

    refactor: pagination 버튼 다 보이게 바꾸고 비활성화 형식으로 변경

--- app/entities/common/Pagination.tsx
+++ app/entities/common/Pagination.tsx
@@ -62,57 +62,63 @@ const Pagination: React.FC<PaginationProps> = ({
     <nav className="flex justify-center mt-12 mb-4">
       <ul className="flex items-center gap-1">
         {/* 처음 페이지 버튼 */}
-        {currentPage > 1 && (
-          <li>
-            <Link
-              href={createPageUrl(1)}
-              className="flex items-center justify-center w-10 h-10 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800"
+        <li>
+          <Link
+            href={createPageUrl(1)}
+            className={`flex items-center justify-center w-10 h-10 rounded-md ${
+              currentPage === 1
+                ? 'text-gray-400 cursor-not-allowed'
+                : 'hover:bg-gray-100 dark:hover:bg-gray-800'
+            }`}
+            onClick={(e) => currentPage === 1 && e.preventDefault()}
+          >
+            <span className="sr-only">처음 페이지</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
             >
-              <span className="sr-only">처음 페이지</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414z"
-                  clipRule="evenodd"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M9.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Link>
-          </li>
-        )}
+              <path
+                fillRule="evenodd"
+                d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414z"
+                clipRule="evenodd"
+              />
+              <path
+                fillRule="evenodd"
+                d="M9.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Link>
+        </li>
 
         {/* 이전 페이지 버튼 */}
-        {currentPage > 1 && (
-          <li>
-            <Link
-              href={createPageUrl(currentPage - 1)}
-              className="flex items-center justify-center w-10 h-10 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800"
+        <li>
+          <Link
+            href={createPageUrl(currentPage - 1)}
+            className={`flex items-center justify-center w-10 h-10 rounded-md ${
+              currentPage === 1
+                ? 'text-gray-400 cursor-not-allowed'
+                : 'hover:bg-gray-100 dark:hover:bg-gray-800'
+            }`}
+            onClick={(e) => currentPage === 1 && e.preventDefault()}
+          >
+            <span className="sr-only">이전 페이지</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
             >
-              <span className="sr-only">이전 페이지</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Link>
-          </li>
-        )}
+              <path
+                fillRule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Link>
+        </li>
 
         {/* 페이지 번호 */}
         {getPageNumbers().map((pageNumber) => (
@@ -131,57 +137,63 @@ const Pagination: React.FC<PaginationProps> = ({
         ))}
 
         {/* 다음 페이지 버튼 */}
-        {currentPage < totalPages && (
-          <li>
-            <Link
-              href={createPageUrl(currentPage + 1)}
-              className="flex items-center justify-center w-10 h-10 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800"
+        <li>
+          <Link
+            href={createPageUrl(currentPage + 1)}
+            className={`flex items-center justify-center w-10 h-10 rounded-md ${
+              currentPage === totalPages
+                ? 'text-gray-400 cursor-not-allowed'
+                : 'hover:bg-gray-100 dark:hover:bg-gray-800'
+            }`}
+            onClick={(e) => currentPage === totalPages && e.preventDefault()}
+          >
+            <span className="sr-only">다음 페이지</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
             >
-              <span className="sr-only">다음 페이지</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Link>
-          </li>
-        )}
+              <path
+                fillRule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Link>
+        </li>
 
         {/* 마지막 페이지 버튼 */}
-        {currentPage < totalPages && (
-          <li>
-            <Link
-              href={createPageUrl(totalPages)}
-              className="flex items-center justify-center w-10 h-10 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800"
+        <li>
+          <Link
+            href={createPageUrl(totalPages)}
+            className={`flex items-center justify-center w-10 h-10 rounded-md ${
+              currentPage === totalPages
+                ? 'text-gray-400 cursor-not-allowed'
+                : 'hover:bg-gray-100 dark:hover:bg-gray-800'
+            }`}
+            onClick={(e) => currentPage === totalPages && e.preventDefault()}
+          >
+            <span className="sr-only">마지막 페이지</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
             >
-              <span className="sr-only">마지막 페이지</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 15.707a1 1 0 001.414 0l5-5a1 1 0 000-1.414l-5-5a1 1 0 00-1.414 1.414L8.586 10 4.293 14.293a1 1 0 000 1.414z"
-                  clipRule="evenodd"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 15.707a1 1 0 001.414 0l5-5a1 1 0 000-1.414l-5-5a1 1 0 00-1.414 1.414L14.586 10l-4.293 4.293a1 1 0 000 1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Link>
-          </li>
-        )}
+              <path
+                fillRule="evenodd"
+                d="M4.293 15.707a1 1 0 001.414 0l5-5a1 1 0 000-1.414l-5-5a1 1 0 00-1.414 1.414L8.586 10 4.293 14.293a1 1 0 000 1.414z"
+                clipRule="evenodd"
+              />
+              <path
+                fillRule="evenodd"
+                d="M10.293 15.707a1 1 0 001.414 0l5-5a1 1 0 000-1.414l-5-5a1 1 0 00-1.414 1.414L14.586 10l-4.293 4.293a1 1 0 000 1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Link>
+        </li>
       </ul>
     </nav>
   );
