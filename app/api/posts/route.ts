20ae4cf085db5d53e73bfd8dc6077286ab8207ac
JeongwooSeo <shipfriend4371@naver.com>
2025-04-09 11:35:59 +0900

    fix: post api 없어졌던거 이제 알았다..

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,6 +3,9 @@ import { QuerySelector } from 'mongoose';
 import Series from '@/app/models/Series';
 import dbConnect from '@/app/lib/dbConnect';
 import Post from '@/app/models/Post';
+import { getServerSession } from 'next-auth';
+import { generateUniqueSlug } from '@/app/lib/utils/post';
+import { getThumbnailInMarkdown } from '@/app/lib/utils/parse';
 
 export async function GET(req: Request) {
   try {
@@ -84,6 +87,78 @@ export async function GET(req: Request) {
     console.error(error);
     return Response.json(
       { success: false, error: '포스트 목록 불러오기 실패', detail: error },
+      { status: 500 }
+    );
+  }
+}
+
+// POST /api/posts - 글 작성 API
+export async function POST(req: Request) {
+  try {
+    const session = await getServerSession();
+
+    if (!session) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    await dbConnect();
+    const {
+      title,
+      subTitle,
+      author,
+      content,
+      profileImage,
+      thumbnailImage,
+      seriesId,
+    } = await req.json();
+
+    if (!title || !content || !author || !content) {
+      return Response.json(
+        { success: false, error: '제목, 소제목, 작성자, 내용은 필수입니다' },
+        { status: 400 }
+      );
+    }
+
+    const post = {
+      slug: await generateUniqueSlug(title, Post),
+      title,
+      subTitle,
+      author,
+      content,
+      timeToRead: Math.ceil(content.length / 500),
+      profileImage,
+      thumbnailImage: thumbnailImage || getThumbnailInMarkdown(content),
+      seriesId: seriesId || null,
+    };
+
+    const newPost = await Post.create(post);
+    if (!newPost) {
+      return Response.json(
+        { success: false, error: '글 작성 실패' },
+        { status: 500 }
+      );
+    }
+
+    if (post.seriesId) {
+      await Series.findByIdAndUpdate(post.seriesId, {
+        $push: { posts: newPost._id },
+        $inc: { postCount: 1 }, // postCount 1 증가
+      });
+    }
+
+    return Response.json(
+      {
+        success: true,
+        post: newPost,
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.error('Post creation error:', error);
+    return Response.json(
+      { success: false, error: 'Invalid Post Request' },
       { status: 500 }
     );
   }
