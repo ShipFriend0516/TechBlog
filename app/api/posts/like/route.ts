75f8aaf4670c6f68053b944c67b815aed23ea75f
JeongwooSeo <shipfriend4371@naver.com>
2025-04-18 23:41:06 +0900

    fix: 좋아요 추가 & 삭제 여부에 따른 응답 추가로 오류 해결

--- app/api/posts/like/route.ts
+++ app/api/posts/like/route.ts
@@ -1,4 +1,5 @@
 import Like from '@/app/models/View';
+import { NextRequest } from 'next/server';
 
 export const POST = async (request: Request) => {
   const { postId } = await request.json();
@@ -13,24 +14,25 @@ export const POST = async (request: Request) => {
 
   const existingLike = await Like.findOne({ postId, fingerprint });
   if (existingLike) {
-    return new Response('이미 조회한 유저입니다.', { status: 203 });
+    return new Response('이미 좋아요한 유저입니다.', { status: 203 });
   } else {
-    await Like.create({
+    const result = await Like.create({
       postId,
       fingerprint,
-    })
-      .then(() => {
-        return new Response('좋아요 증가 성공', { status: 200 });
-      })
-      .catch((error) => {
-        console.error('좋아요 증가 오류:', error);
-        return new Response('좋아요 증가 실패', { status: 500 });
-      });
+    });
+
+    if (result) {
+      return Response.json({ message: '좋아요 성공' }, { status: 200 });
+    } else {
+      return Response.json({ message: '좋아요 실패' }, { status: 500 });
+    }
   }
 };
 
 export const DELETE = async (request: Request) => {
-  const { postId, fingerprint } = await request.json();
+  const { postId } = await request.json();
+  const fingerprint = request.headers.get('X-Fingerprint') || '';
+
   if (!postId) {
     return new Response('postId가 필요합니다.', { status: 400 });
   }
@@ -42,13 +44,43 @@ export const DELETE = async (request: Request) => {
   if (!existingLike) {
     return new Response('좋아요가 되어있지 않습니다.', { status: 203 });
   } else {
-    await Like.deleteOne({ postId, fingerprint })
-      .then(() => {
-        return new Response('좋아요 취소 성공', { status: 200 });
-      })
-      .catch((error) => {
-        console.error('좋아요 취소 오류:', error);
-        return new Response('좋아요 취소 실패', { status: 500 });
-      });
+    const result = await Like.deleteOne({ postId, fingerprint });
+
+    if (result) {
+      return new Response('좋아요 취소 성공', { status: 200 });
+    } else {
+      return new Response('좋아요 취소 실패', { status: 500 });
+    }
   }
+};
+
+export const GET = async (request: NextRequest) => {
+  const postId = request.nextUrl.searchParams.get('postId') || '';
+  const fingerprint = request.headers.get('X-Fingerprint') || '';
+
+  if (!postId) {
+    return new Response('postId가 필요합니다.', { status: 400 });
+  }
+  const likeCount = (await Like.countDocuments({ postId })) || 0;
+
+  if (!fingerprint) {
+    return Response.json(
+      {
+        message: 'Fingerprint가 없으면 좋아요 여부를 확인할 수 없습니다.',
+        isLiked: false,
+        likeCount: likeCount,
+      },
+      { status: 400 }
+    );
+  }
+
+  const isLiked = await Like.findOne({ postId, fingerprint });
+
+  return Response.json(
+    {
+      isLiked: isLiked,
+      likeCount: likeCount,
+    },
+    { status: 200 }
+  );
 };
