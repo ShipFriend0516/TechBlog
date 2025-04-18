043afff15ad6d3733cfc3efdc5d65feb6880818e
JeongwooSeo <shipfriend4371@naver.com>
2025-04-18 20:28:08 +0900

    fix: google search console 인증용 파일을 api 형식 대신 public 폴더에 저장

--- app/googlee045d7c5db6fc750.html/route.ts
+++
@@ -1,9 +0,0 @@
-export const GET = () => {
-  const content = 'google-site-verification: googlee045d7c5db6fc750.html';
-
-  return new Response(content, {
-    headers: {
-      'Content-Type': 'text/html',
-    },
-  });
-};
