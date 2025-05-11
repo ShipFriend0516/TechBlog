9650c30a2ad3301ec33da1bca81cfccf00b0307a
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2025-05-11 13:54:15 +0900

    fix: useDataFetch 훅 config 변경시 재실행되도록 로직 수정 및 제네럴 타입 수정

--- app/hooks/useDataFetch.ts
+++ app/hooks/useDataFetch.ts
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import axios, { AxiosError, AxiosRequestConfig, Method } from 'axios';
 
-interface useDataFetchProps<T = any> {
+export interface useDataFetchConfig<T = any> {
   url: string;
   method: Method;
   body?: Record<string, any>;
   config?: AxiosRequestConfig;
-  onSuccess?: (data: any) => void;
+  onSuccess?: (data: T) => void;
   onError?: (error: any) => void;
   onLoading?: () => void;
 }
@@ -19,8 +19,8 @@ const useDataFetch = <T = never>({
   onLoading,
   onSuccess,
   onError,
-}: useDataFetchProps<T>) => {
-  const [loading, setLoading] = useState(false);
+}: useDataFetchConfig<T>) => {
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState<AxiosError | Error | null>(null);
   const [data, setData] = useState<T | null>();
 
@@ -31,38 +31,39 @@ const useDataFetch = <T = never>({
     ...config,
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        setError(null);
+  const fetchData = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError(null);
 
-        const response = await axios(axiosConfig);
-        const data = response.data;
+      const response = await axios(axiosConfig);
+      const data = response.data;
 
-        setData(data);
-        setLoading(false);
-        if (onSuccess) {
-          onSuccess(data);
-        }
-        if (onLoading) {
-          onLoading();
-        }
-      } catch (e) {
-        if (axios.isAxiosError(e)) {
-          console.error('데이터를 Fetch 하는데에 실패했습니다.', e);
-          setError(e);
-          setLoading(false);
-          if (onError) {
-            onError(e);
-          }
-        } else {
-          console.error('Unknown Error:', e);
-        }
+      setData(data);
+      setLoading(false);
+      if (onSuccess) {
+        onSuccess(data);
       }
-    };
+      if (onLoading) {
+        onLoading();
+      }
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        console.error('데이터를 Fetch 하는데에 실패했습니다.', e);
+        setError(e);
+        setLoading(false);
+        if (onError) {
+          onError(e);
+        }
+      } else {
+        console.error('Unknown Error:', e);
+      }
+    }
+  }, [url, method, body, config, onSuccess, onError, onLoading]);
+
+  useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);
 
   return { data, loading, error };
 };
