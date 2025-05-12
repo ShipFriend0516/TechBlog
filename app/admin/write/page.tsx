c381236b724ba203a8b0e5b23da25197e5bf91de
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2025-05-13 00:28:20 +0900

    chore: 폰트 사이즈 축소

--- app/admin/write/page.tsx
+++ app/admin/write/page.tsx
@@ -3,7 +3,7 @@ import BlogForm from '@/app/entities/post/write/BlogForm';
 const BlogWritePage = () => {
   return (
     <section className={'p-6 max-w-7xl mx-auto'}>
-      <h1 className={'text-3xl text-center mb-4'}>글 작성</h1>
+      <h1 className={'text-2xl text-center mb-4'}>글 작성</h1>
       <BlogForm />
     </section>
   );
