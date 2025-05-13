d92ecaa74e56be5c67a8afedd63fef61824c4661
JeongwooSeo <shipfriend4371@naver.com>
2025-05-13 17:03:45 +0900

    feat: Overlay hoc 화면 중앙에 뜨게 수정

--- app/entities/common/Overlay/Overlay.tsx
+++ app/entities/common/Overlay/Overlay.tsx
@@ -28,9 +28,9 @@ const Overlay = ({
     overlayOpen && (
       <div
         ref={overlayRef}
-        className="fixed inset-0 bg-black bg-opacity-50 z-50"
+        className="fixed flex justify-center items-center w-screen h-screen top-0 left-0 inset-0 bg-black bg-opacity-50 z-50"
       >
-        <div className="animate-popUp  container bg-opacity-90 text-overlay rounded-lg mx-auto mt-[24%] max-w-2xl">
+        <div className="animate-popUp  container bg-opacity-90 text-overlay rounded-lg mx-auto max-w-2xl">
           {children}
         </div>
       </div>
