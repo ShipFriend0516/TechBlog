cbb4d20e22c2da51c59da784530ea4efaeda8afd
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2025-06-01 18:12:16 +0900

    feat: series page skeleton ui 추가

---
+++ app/entities/common/Skeleton/SeriesPreviewSkeleton.tsx
@@ -0,0 +1,42 @@
+import Skeleton from '@/app/entities/common/Skeleton/Skeleton';
+import React from 'react';
+
+const SeriesPreviewSkeleton = () => {
+  const darkmodeStyle = `dark:bg-neutral-900 dark:text-neutral-200 dark:border-neutral-800`;
+  const lightmodeStyle = `bg-white border-slate-200`;
+
+  return (
+    <div
+      className={`rounded-lg shadow-sm overflow-hidden border ${lightmodeStyle} ${darkmodeStyle}`}
+    >
+      {/* 썸네일 스켈레톤 */}
+      <div className="relative aspect-video w-full overflow-hidden">
+        <Skeleton className="w-full h-full" />
+      </div>
+
+      <div className="p-5">
+        {/* 제목 스켈레톤 */}
+        <div className="mb-2">
+          <Skeleton className="h-6 w-3/5 mb-2" />
+        </div>
+
+        {/* 날짜와 포스트 수 스켈레톤 */}
+        <div className="flex items-center gap-2 mb-3">
+          <Skeleton className="h-3 w-24" />
+          <div className="ml-auto">
+            <Skeleton className="h-4 w-16" />
+          </div>
+        </div>
+
+        {/* 설명 스켈레톤 */}
+        <div className="space-y-2">
+          <Skeleton className="h-3 w-full" />
+          <Skeleton className="h-3 w-full" />
+          <Skeleton className="h-3 w-2/3" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SeriesPreviewSkeleton;
--- app/series/page.tsx
+++ app/series/page.tsx
@@ -5,6 +5,8 @@ import { getAllSeriesData } from '@/app/entities/series/api/series';
 import { Series } from '@/app/types/Series';
 import SeriesPreview from '@/app/entities/series/list/SeriesPreview';
 import SVGLoadingSpinner from '@/app/entities/common/Loading/SVGLoadingSpinner';
+import Skeleton from '@/app/entities/common/Skeleton/Skeleton';
+import SeriesPreviewSkeleton from '@/app/entities/common/Skeleton/SeriesPreviewSkeleton';
 
 const SeriesListPage = () => {
   const [series, setSeries] = useState<Series[]>([]);
@@ -34,13 +36,15 @@ const SeriesListPage = () => {
   return (
     <section className={'w-full p-10 max-w-5xl mx-auto'}>
       <h1 className={'text-4xl font-bold mt-4'}>시리즈</h1>
-      <p className={'text-lg mb-4'}>시리즈별로 글을 확인해보세요.</p>
+      <p className={'text-lg mb-4'}>
+        시리즈별로 글을 확인해보세요. 클릭시 세부 페이지로 이동합니다.
+      </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {loading ? (
-          <SVGLoadingSpinner message={'시리즈를 불러오는 중...'} />
-        ) : (
-          series.map((item) => <SeriesPreview item={item} key={item.slug} />)
-        )}
+        {loading
+          ? Array.from({ length: 6 }).map((_, index) => (
+              <SeriesPreviewSkeleton key={index} />
+            ))
+          : series.map((item) => <SeriesPreview item={item} key={item.slug} />)}
       </div>
     </section>
   );
