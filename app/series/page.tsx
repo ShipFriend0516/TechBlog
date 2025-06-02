eabea9a40344bf66c1d090dc066a474e4b318fbc
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2025-06-02 21:31:48 +0900

    style: series page 반응형 조절

--- app/series/page.tsx
+++ app/series/page.tsx
@@ -32,9 +32,9 @@ const SeriesListPage = () => {
   }
 
   return (
-    <section className={'w-full p-10 max-w-5xl mx-auto'}>
+    <section className={'w-full p-4 max-w-5xl mx-auto'}>
       <h1 className={'text-4xl font-bold mt-4'}>시리즈</h1>
-      <p className={'text-lg mb-4'}>
+      <p className={'text-lg text-weak mb-4'}>
         시리즈별로 글을 확인해보세요. 클릭시 세부 페이지로 이동합니다.
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
