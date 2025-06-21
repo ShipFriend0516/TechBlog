dfa650b26a85357a42b8fbb34400167761b1ab23
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2025-06-22 01:21:14 +0900

    chore: console.log 삭제

--- app/entities/post/detail/PostRecommendation.tsx
+++ app/entities/post/detail/PostRecommendation.tsx
@@ -3,11 +3,8 @@ import useDataFetch, {
   useDataFetchConfig,
 } from '@/app/hooks/common/useDataFetch';
 import { Post } from '@/app/types/Post';
-import Image from 'next/image';
-
 import PostRecommendationListItem from '@/app/entities/post/detail/PostRecommendationListItem';
 import ErrorBox from '@/app/entities/common/Error/ErrorBox';
-import { CiBookmark } from 'react-icons/ci';
 import { FaBookmark } from 'react-icons/fa';
 import Skeleton from '@/app/entities/common/Skeleton/Skeleton';
 interface PostRecommendationProps {
@@ -15,10 +12,6 @@ interface PostRecommendationProps {
   currentPostId: string;
   seriesId: string;
 }
-
-// 포스트 추천 설계하기
-// 1. 추천 포스트를 가져오는 API를 만들어야 한다.
-// 태그를 전달해서 추천 글 3개를 가져오는 API를 만들지 아니면 기존 get 메서드를 활용해서 tag 검색 후 최근 3개 포스트를 가져오는게 좋을지
 
 interface PostRecommendationResponse {
   posts: Post[];
@@ -39,13 +32,10 @@ const PostRecommendation = ({
         seriesId: seriesId,
       },
     },
-    // dependencies: [tags],
   };
 
   const { data, loading, error } =
     useDataFetch<PostRecommendationResponse>(config);
-
-  console.log(data);
 
   return (
     <div className={'max-w-3xl mx-auto mt-4 px-2'}>
