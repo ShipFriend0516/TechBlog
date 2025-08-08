ed46b01a08b99d3ab53722107285494039bd30a2
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2025-08-08 17:59:17 +0900

    feat: useShortcut 훅으로 단축키 기능 선언적으로 구현

---
+++ app/hooks/common/useShortcut.ts
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+
+interface useShortcutProps {
+  onShortcutAction: () => void;
+  keys: string[];
+}
+
+const useShortcut = ({ onShortcutAction, keys }: useShortcutProps) => {
+  useEffect(() => {
+    const handler = (e) => {
+      const isPressed = keys?.every((key) => {
+        if (key === 'Alt') {
+          return e.altKey;
+        } else if (key === 'Control' || key === 'Ctrl') {
+          return e.ctrlKey;
+        } else if (key === 'Shift') {
+          return e.shiftKey;
+        } else if (key === 'Meta') {
+          return e.metaKey;
+        }
+        return e.key.toLowerCase() === key.toLowerCase();
+      });
+      if (isPressed) {
+        if (onShortcutAction) {
+          onShortcutAction();
+        }
+      }
+    };
+
+    if (window) {
+      document.addEventListener('keydown', handler);
+    }
+    return () => {
+      document.removeEventListener('keydown', handler);
+    };
+  }, [onShortcutAction, keys]);
+
+  return;
+};
+
+export default useShortcut;
