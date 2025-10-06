d4b0da0c6e3c3ee17d60eb6aef3916ec031533a7
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2025-10-06 19:26:43 +0900

    refactor: 토스트 메시지가 스택으로 쌓이도록 수정

--- app/entities/common/Toast/Toast.tsx
+++ app/entities/common/Toast/Toast.tsx
@@ -15,23 +15,19 @@ const Toast = ({ message, type, removeToast }: ToastProps) => {
     }
   };
   return (
-    <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50 w-[90%] max-w-md">
-      <div
-        onClick={() => removeToast()}
-        className={`
+    <div
+      onClick={() => removeToast()}
+      className={`
           transform transition-all duration-300 ease-out animate-slideUp
           bg-gray-200/90 text-black px-4 py-3 rounded-lg flex items-center gap-3 
           backdrop-blur-sm w-full max-w-md origin-center cursor-pointer 
           hover:bg-gray-300/90 hover:shadow-lg
         `}
-      >
-        <div className={`flex items-center justify-center rounded-full p-0.5`}>
-          {iconRender(type)}
-        </div>
-        <p className="text line-clamp-1 whitespace-pre-line flex-1">
-          {message}
-        </p>
+    >
+      <div className={`flex items-center justify-center rounded-full p-0.5`}>
+        {iconRender(type)}
       </div>
+      <p className="text line-clamp-1 whitespace-pre-line flex-1">{message}</p>
     </div>
   );
 };
