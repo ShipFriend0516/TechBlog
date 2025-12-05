b0f09d06643bcad718440c1374713e2211eaf7f8
sjw4371 <sjw4371@cj.net>
2025-12-05 22:30:30 +0900

    feat: 버튼 아이콘 추가

--- app/entities/post/write/PostWriteButtons.tsx
+++ app/entities/post/write/PostWriteButtons.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { FaHome } from 'react-icons/fa';
+import { FaChevronLeft, FaCloud, FaRocket } from 'react-icons/fa6';
 import LoadingSpinner from '@/app/entities/common/Loading/LoadingSpinner';
 import { PostBody } from '@/app/types/Post';
 
@@ -18,35 +20,46 @@ const PostWriteButtons = ({
   saveToDraft,
   saveToCloud,
 }: PostWriteButtonsProps) => {
-  const buttonStyle = `font-bold py-2 px-4 rounded mr-2 disabled:bg-opacity-75 `;
+  const buttonStyle = `inline-flex justify-center items-center font-bold py-2 px-4 rounded disabled:bg-opacity-75 gap-2 `;
   return (
-    <div className={'w-full flex justify-center my-6'}>
-      <Link
-        href={'/admin'}
-        className={buttonStyle + ' text-black bg-gray-200 hover:bg-red-500 '}
-      >
-        <button>나가기</button>
+    <div className={'w-full flex justify-center my-6 gap-2'}>
+      <Link href={'/admin'}>
+        <button
+          className={buttonStyle + 'text-black bg-gray-200 hover:bg-red-500 '}
+        >
+          <FaChevronLeft />
+          나가기
+        </button>
       </Link>
       <button
         onClick={saveToDraft}
-        className={buttonStyle + 'bg-blue-500 hover:bg-blue-700 '}
+        className={buttonStyle + 'bg-blue-500 hover:bg-blue-700 text-white '}
       >
-        임시 저장
+        <FaHome />
+        저장
       </button>
       <button
         onClick={saveToCloud}
-        className={buttonStyle + 'bg-green-500 hover:bg-green-700 '}
+        className={
+          buttonStyle +
+          'text-white bg-primary-mountain/80 hover:bg-primary-mountain '
+        }
       >
-        클라우드에 저장
+        <FaCloud />
+        저장
       </button>
       <button
         disabled={submitLoading}
-        className={buttonStyle + 'bg-emerald-500  hover:bg-emerald-700 '}
+        className={
+          buttonStyle +
+          'bg-primary-bangladesh/80  hover:bg-primary-bangladesh text-white'
+        }
         onClick={(e) => {
           e.preventDefault();
           submitHandler(postBody);
         }}
       >
+        <FaRocket />
         {submitLoading ? <LoadingSpinner /> : slug ? '글 수정' : '글 발행'}
       </button>
     </div>
