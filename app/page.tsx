4ebaf5992ed427d5f443b6f9b4d7ed04496b9a49
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2026-02-14 23:22:24 +0900

    fix: 메인페이지 단축키 제거

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import useDataFetch from '@/app/hooks/common/useDataFetch';
-import useShortcut from '@/app/hooks/common/useShortcut';
 import useFingerprint from '@/app/hooks/useFingerprint';
 import useToast from '@/app/hooks/useToast';
 import { projects } from '@/app/lib/constants/landingPageData';
@@ -38,19 +37,6 @@ export default function Home() {
       toast.success('다시 오신 것을 환영합니다!');
     }
   }, [fingerprint]);
-
-  const goToWritePage = () => {
-    toast.success('글쓰기 페이지로 이동합니다...');
-    router.push('/admin/write');
-  };
-
-  const goToPostsPage = () => {
-    toast.success('모든 글 목록 페이지로 이동합니다...');
-    router.push('/posts');
-  };
-
-  useShortcut(goToWritePage, ['Alt', 'N'], true);
-  useShortcut(goToPostsPage, ['Ctrl', ';'], true);
 
   return (
     <main className="w-full max-w-5xl mx-auto grid gap-12 p-4 md:p-6">
