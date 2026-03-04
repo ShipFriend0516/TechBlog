47c08a8398cd00a529207645f2e549af542bd8eb
JeongwooSeo <sjw4371@cj.net>
2026-03-04 15:41:08 +0900

    fix: 포트폴리오 프로젝트 순서 변경

--- app/portfolio/data.ts
+++ app/portfolio/data.ts
@@ -9,15 +9,7 @@ export const projects: Project[] = [
     githubUrl: 'https://github.com/boostcampwm-2024/web27-Preview',
     slug: 'preview',
   },
-  {
-    title: 'Primitive',
-    description: '프로그래밍 동아리 PRIMITIVE 홍보 및 프로젝트 공유 플랫폼',
-    image: '/images/logo/primitive-logo.webp',
-    tags: ['React', 'TypeScript', 'Firebase'],
-    demoUrl: 'https://primitive.kr',
-    githubUrl: 'https://github.com/ShipFriend0516/Primitive',
-    slug: 'primitive',
-  },
+
   {
     title: 'ShipFriend TechBlog',
     description: '개인 개발 블로그',
@@ -36,5 +28,14 @@ export const projects: Project[] = [
       'https://chromewebstore.google.com/detail/storyhelper/inmbdknioncgblpeiiohmdihhidnjpfp?authuser=0&hl=ko',
     githubUrl: 'https://github.com/ShipFriend0516/StoryHelper',
     slug: 'storyhelper',
+  },
+  {
+    title: 'Primitive',
+    description: '프로그래밍 동아리 PRIMITIVE 홍보 및 프로젝트 공유 플랫폼',
+    image: '/images/logo/primitive-logo.webp',
+    tags: ['React', 'TypeScript', 'Firebase'],
+    demoUrl: 'https://primitive.kr',
+    githubUrl: 'https://github.com/ShipFriend0516/Primitive',
+    slug: 'primitive',
   },
 ];
